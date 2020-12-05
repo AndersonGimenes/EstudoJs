09a2e5587bc1c537025646777eddb40988d41051
AndersonGimenes <gcosta19@gmail.com>
2020-12-05 05:32:51 -0300

    Ajustes classe DataHelper

--- JS-II/client/js/app/helper/DataHelper.js
+++ JS-II/client/js/app/helper/DataHelper.js
@@ -1,5 +1,12 @@
 class DataHelper{
+
+    constructor(){
+        throw new Error('Classe não deve ser instanciada!');
+    }
     static dataParaTexto(data){
+        if(!/\d{4}-\d{2}-\d{2}/.test(data))
+            alert('Data no formato invalido');
+
         return `${data.getDate()}/${data.getMonth() + 1 }/${data.getFullYear()}`;
     }
 
